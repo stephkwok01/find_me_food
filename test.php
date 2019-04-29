553197e2216f8290131bbfb99f53471f5af2f1b7
Stephanie Kwok <skwok@bu.edu>
2019-04-28 20:43:44 -0400

    added logging in tests

--- test.php
+++ test.php
@@ -172,41 +172,36 @@ $complete_res = check_data_completeness();
 
 
 /** 
- * TODO
+ * Test api call with wrong credentials
  */
 function testapi(){
-$url = 'http://charles.plumgroup.com/~skwok/personal/find_me_food/api.php';
+  GLOBAL $LOGFILE;
+  //Open log file 
+  $loghandle = fopen('logs/'.$LOGFILE, 'a+') or die('cannot open file');
+  fwrite($loghandle, "----------Test API call with wrong credentials: testapi()----------\n");
+  $url = 'http://charles.plumgroup.com/~skwok/personal/find_me_food/api.php';
 
-$username = 'wronguser';
-$password = 'asdf';
+  $username = 'wronguser';
+  $password = 'asdf';
 
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
-curl_setopt($ch, CURLOPT_USERPWD, $username.":".$password);
-curl_setopt($ch, CURLOPT_HTTPHEADER, array("Accept: application/json", "Content-type: application/json"));
-curl_setopt($ch, CURLOPT_URL, $url);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+  $ch = curl_init();
+  curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+  curl_setopt($ch, CURLOPT_USERPWD, $username.":".$password);
+  curl_setopt($ch, CURLOPT_HTTPHEADER, array("Accept: application/json", "Content-type: application/json"));
+  curl_setopt($ch, CURLOPT_URL, $url);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 
-$result = json_decode(curl_exec($ch),true);
-return $result;
+  $result = json_decode(curl_exec($ch),true);
+  if($result['error']){
+    fwrite($loghandle, "Unauthorized Access; test successful");
+    return true;
+  }
+  fwrite($loghandle, "authentication failed");
+  return false;
+ 
 }
 $result = testapi();
-($result['error'] == 'Unauthorized Access') ? var_dump('test successful') : var_dump("Test failed");
 
-
-//Test query 
-function testquery(){
-DBConnect();
-$result = DBQuery(
-  "SELECT restaurant_id, name, boro, building, street, zipcode, phone, MAX(grade_date) as 'grade_date', grade FROM restaurant
-  JOIN cuisine ON FK_cuisine_id = cuisine_id
-  JOIN inspection ON restaurant_id = FK_restaurant_id
-  WHERE (grade = 'A' OR grade = 'B') AND cuisine.description = 'thai'
-  GROUP BY restaurant_id");
-return $result;
-}
-$result = testquery();
-(count($result) == 289) ? var_dump('success') : var_dump("fail");
 
  
 
